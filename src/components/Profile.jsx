07e935cef452351b14ae0405173d8e79b963f2ec
Гавриш Олександр <132341505+Lapik542@users.noreply.github.com>
2024-02-08 15:03:06 +0200

    main

---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+export const Profile = ({
+   data:{ name, tag, location, image, stats
+}}) => {
+  return (
+    <div>
+      <div>
+        <img src={image} alt="User avatar" />
+        <p>{name}</p>
+        <p>@{tag}</p>
+        <p>{location}</p>
+      </div>
+      <ul>
+        <li>
+          <span>Followers</span>
+          <span>{stats}</span>
+        </li>
+        <li>
+          <span>Views</span>
+          <span>{stats}</span>
+        </li>
+        <li>
+          <span>Likes</span>
+          <span>{stats}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
